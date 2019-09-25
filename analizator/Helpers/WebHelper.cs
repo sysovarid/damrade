0fdeb3546c4f2287e638ed533028d455c2b0337b
kle1n.luk4 <kle1n.luk4@gmail.com>
2019-09-25 19:42:52 +0200

    Added Helpers
    CountHelper - for count various data.
    WebHelper - for connect with website and download content.

---
+++ analizator/Helpers/WebHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace analizator.Helpers
+{
+    public class WebHelper
+    {
+        private string ConnectionPath { get; set; }
+
+        private string Content { get; set; }
+
+        private WebClient Client { get; set; }
+
+        public WebHelper()
+        {
+            ConnectionPath = ConfigurationManager.AppSettings["address"];
+            Client = new WebClient();
+        }
+
+        public string GetAllContentToList()
+        {
+            try
+            {
+
+               Content = Client.DownloadString(ConnectionPath);
+               return Content;
+
+            }
+            catch (WebException e)
+            {
+                return $"Somethink went wrong! Message: {e.Message}";
+            }
+
+        }
+    }
+}
