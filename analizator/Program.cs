921732bcd4d1fec681dc800cb3bc8f59f3b11502
sysovarid <robert.p@wp.pl>
2019-09-27 18:20:05 +0200

    Fixes #1

--- analizator/Program.cs
+++ analizator/Program.cs
@@ -10,7 +10,27 @@ namespace analizator
     {
         static void Main(string[] args)
         {
-
+            while (true)
+            {
+                Console.WriteLine("1. Pobierz plik z internetu");
+                Console.WriteLine("2. Zlicz liczbę liter w pobranym pliku");
+                Console.WriteLine("3. Zlicz liczbę wyrazów w pliku");
+                Console.WriteLine("4. Zlicz liczbę znaków interpunkcyjnych w pliku");
+                Console.WriteLine("5. Zlicz liczbę zdań w pliku");
+                Console.WriteLine("6. Wygeneruj raport o użyciu liter");
+                Console.WriteLine("7. Zapisz statystyki z punktów 2-5 do pliku statystyki.txt");
+                Console.WriteLine("8. Wyjście z programu");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 1) ;
+                if (menuOption == 2) ;
+                if (menuOption == 3) ;
+                if (menuOption == 4) ;
+                if (menuOption == 5) ;
+                if (menuOption == 6) ;
+                if (menuOption == 7) ;
+                if (menuOption == 8) ;
+                break;
+            }
         }
     }
 }
